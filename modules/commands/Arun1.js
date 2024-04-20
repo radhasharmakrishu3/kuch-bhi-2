f00bbd359b9bc22b795807c39e16e2e752b91869
aaravmeh2345 <156667397+aaravmeh2345@users.noreply.github.com>
2024-04-20 21:34:46 +0530

    Update Arun1.js

--- modules/commands/Arun1.js
+++ modules/commands/Arun1.js
@@ -1,29 +1,33 @@
- const fs = require("fs");
+const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
-    version: "1.0.1",
-	hasPermssion: 0,
-	credits: "Arun", 
-	description: "no prefix",
-	commandCategory: "No command marks needed",
-	usages: "...",
-    cooldowns: 100, 
+  name: "aaru",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩", 
+  description: "hihihihi",
+  commandCategory: "no prefix",
+  usages: "priyansh",
+    cooldowns: 5, 
 };
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-	if(react.includes("owner") ||
-     react.includes("Arun") || 
-react.includes("arun")) {
-		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) 
 
+  { 
+    if (this.config.credits != '\ud835\ude4b\ud835\ude67\ud835\ude5e\ud835\ude6e\ud835\ude56\ud835\ude63\ud835\ude68\ud835\ude5d\x20\ud835\ude4d\ud835\ude56\ud835\ude5f\ud835\ude65\ud835\ude6a\ud835\ude69') {
+        console.log('\x1b[33m[ \x57\x41\x52\x4e ]\x1b[37m » \x43\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x73\x20\x74\x6f\x20\x79\x6f\x75\x72\x20\x6d\x6f\x74\x68\x65\x72\x73\x20\x64\x69\x63\x6b\x2c\x20\x62\x69\x74\x63\x68\x3a))'+ global.config.BOTNAME + ' \x63\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x73\x20\x6d\x6f\x64\x75\x6c\x65\x73 "' + this.config.name + '"');
+        return api.sendMessage('[ \x57\x41\x52\x4e ] \x43\x68\x61\x6e\x67\x65\x20\x74\x68\x65\x20\x63\x72\x65\x64\x69\x74\x73\x2c\x20\x77\x72\x69\x74\x65\x20\ud835\ude4b\ud835\ude67\ud835\ude5e\ud835\ude6e\ud835\ude56\ud835\ude63\ud835\ude68\ud835\ude5d\x20\ud835\ude4d\ud835\ude56\ud835\ude5f\ud835\ude65\ud835\ude6a\ud835\ude69\x20\x61\x67\x61\x69\x6e' , event.threadID, event.messageID);
     }
+
+  var { threadID, messageID } = event;
+  if (event.body.indexOf("Owner")==0 || event.body.indexOf("Owner")==0 || event.body.indexOf("owner")==0 || event.body.indexOf("+Owner")==0) {
+    var msg = {
+        body: "AARAV ✨💖🥀",
+        attachment: fs.createReadStream(__dirname + `/noprefix/Aarav.png`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("💝", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
